813454972b92726bbed5721d8849ade5bc9e2471
siatheindochinese <commander_sia@hotmail.com>
2024-01-29 20:02:29 -0500

    added inference for files and buffer extraction

---
+++ file_inference.py
@@ -0,0 +1,201 @@
+import numpy as np
+from collections import OrderedDict
+from queue import Queue
+import argparse
+import cv2
+import torch
+
+# import YOLOX functions and attributes
+# NOTE: find a way to remove redundant files
+from yolox import yolox_nano
+from yolox.data.data_augment import ValTransform
+#from yolox.data.datasets import COCO_CLASSES
+from yolox.utils import postprocess, vis
+from yolox.utils.visualize import plot_tracking
+
+# import BYTETracker
+from bytetrack import BYTETracker
+
+# import ViFi-CLIP
+from vificlip.utils.config import get_config
+from vificlip.trainers import vificlip
+from vificlip.datasets.pipeline import Compose
+
+parser = argparse.ArgumentParser(description ='Person detection, tracking and action recogniton.')
+parser.add_argument("-v", "--videopath",
+					type = str,
+					help ='path to video you wish to perform inference on')
+parser.add_argument("-ht", "--height",
+					type = int,
+					help ='height of video')
+parser.add_argument("-wt", "--width",
+					type = int,
+					help ='width of video')
+args = parser.parse_args()
+
+################################################################
+# load yolox model and weights here                            #
+# COMMENT: these parameters are hardcoded, might wanna offload #
+#          offload them into a separate config file            #
+################################################################
+test_size = (512, 512)
+test_conf = 0.01
+nmsthre = 0.65
+num_classes = 80
+
+model = yolox_nano.Exp().get_model()
+ckpt = torch.load('weights/yolox_nano.pth', map_location="cpu")
+model.load_state_dict(ckpt["model"])
+model.cuda()
+model.half()
+model.eval()
+
+# yolox utils
+preproc = ValTransform(legacy=False)
+
+########################
+# Configure cv2 stream #
+########################
+cap = cv2.VideoCapture(args.videopath)
+width, height = args.width, args.height
+
+cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+
+####################
+# load BYTETracker #
+####################
+aspect_ratio_thresh = 0.6
+min_box_area = 10
+track_thresh = 0.5
+track_buffer = 60
+match_thresh = 0.8
+fuse_score = False
+frame_rate = 30
+
+tracker = BYTETracker(track_thresh=track_thresh,
+					  track_buffer=track_buffer,
+					  match_thresh=match_thresh,
+					  fuse_score=fuse_score,
+					  frame_rate=frame_rate)
+
+################################################################
+# load ViFi-CLIP, config and weights here                      #
+# COMMENT: these parameters are hardcoded, might wanna offload #
+#          offload them into a separate config file            #
+################################################################
+class_names = ['punch','kick','walk']
+
+class parse_option():
+    def __init__(self):
+        self.config = 'vificlip/configs/zero_shot/train/k400/16_16_vifi_clip.yaml'
+        self.output =  ''
+        self.resume = 'weights/vifi_clip_10_epochs_k400_full_finetuned.pth'
+        # No need to change below args.
+        self.only_test = True
+        self.opts = None
+        self.batch_size = None
+        self.pretrained = None
+        self.accumulation_steps = None
+        self.local_rank = 0
+vargs = parse_option()
+vconfig = get_config(vargs)
+
+vmodel = vificlip.returnCLIP(vconfig,
+                            class_names=class_names,)
+vmodel.cuda()
+vmodel.eval()
+vmodel.half()
+
+vckpt = torch.load(vconfig.MODEL.RESUME, map_location='cpu')
+load_state_dict = vckpt['model']
+# now remove the unwanted keys:
+if "module.prompt_learner.token_prefix" in load_state_dict:
+	del load_state_dict["module.prompt_learner.token_prefix"]
+
+if "module.prompt_learner.token_suffix" in load_state_dict:
+	del load_state_dict["module.prompt_learner.token_suffix"]
+
+if "module.prompt_learner.complete_text_embeddings" in load_state_dict:
+	del load_state_dict["module.prompt_learner.complete_text_embeddings"]
+
+new_state_dict = OrderedDict()
+for k, v in load_state_dict.items():
+	name = k[7:] # remove `module.`
+	new_state_dict[name] = v
+
+vmodel.load_state_dict(new_state_dict, strict=False)
+
+###################
+# Start streaming #
+###################
+# init global buffer to collect frames
+global_buffer = Queue(32)
+# init local buffer to collect metadata from each tracked ID
+# COMMENT: choose between limited/unlimited number of local buffers
+#          limited to 20 local buffers for now
+local_buffers = dict()
+
+with torch.no_grad():
+	while True:
+		# Wait for color frame
+		ret, img = cap.read() 
+		if not ret:
+			break
+
+		# BGR?? RGB?? Double check if channels are correct
+		#global_buffer.put(img)
+
+		# inference
+		img_ori = img
+		ratio = min(test_size[0] / height, test_size[1] / width)
+		img, _ = preproc(img, None, test_size)
+		img = torch.from_numpy(img).unsqueeze(0).float().half().cuda()
+		outputs = model(img)
+		outputs = postprocess(outputs,
+							  num_classes,
+							  test_conf,
+							  nmsthre,
+							  class_agnostic=True)
+
+		# BYTETracker
+		if outputs[0] is not None:
+			output = outputs[0].cpu()
+			output = output[output[:, 6] == 0] # filter out ONLY persons
+
+			online_targets = tracker.update(output, [height, width], test_size)
+			online_tlwhs = []
+			online_ids = []
+			online_scores = []
+			for t in online_targets:
+				tlwh = t.tlwh
+				tid = t.track_id
+				vertical = tlwh[2] / tlwh[3] > aspect_ratio_thresh
+				if tlwh[2] * tlwh[3] > min_box_area and not vertical:
+					online_tlwhs.append(tlwh)
+					online_ids.append(tid)
+					online_scores.append(t.score)
+					tlbr = tlwh + np.array([0, 0, tlwh[0]+tlwh[2], tlwh[1]+tlwh[3]])
+					if tid not in local_buffers:
+						local_buffers[tid] = tlbr
+					else:
+						old_tlbr = local_buffers.get(tid)
+						tmpmsk = old_tlbr < tlbr
+						local_buffers[tid][tmpmsk] = tlbr[tmpmsk]
+					vis_res = plot_tracking(img_ori, online_tlwhs, online_ids)
+		else:
+			vis_res = img_ori
+		global_buffer.put(vis_res)
+
+		if global_buffer.full():
+			print('local buffers')
+			for tid in local_buffers:
+				print(tid, local_buffers.get(tid))
+			print('')
+			local_buffers.clear()
+			while not global_buffer.empty():
+				cv2.imshow('clip', global_buffer.get())
+				cv2.waitKey(1)
+		# Only show images if p
+		if cv2.waitKey(1) & 0xff == ord('q'):
+			break
+cap.release()
--- inference.py
+++ inference.py
@@ -1,10 +1,12 @@
 import pyrealsense2 as rs
 import numpy as np
+from collections import OrderedDict
+from queue import Queue
 import cv2
 import torch
 
-#import YOLOX functions and attributes
-# NOTE: find a way to remove redundant yolo files and keep yolox-nano only
+# import YOLOX functions and attributes
+# NOTE: find a way to remove redundant files
 from yolox import yolox_nano
 from yolox.data.data_augment import ValTransform
 #from yolox.data.datasets import COCO_CLASSES
@@ -14,9 +16,16 @@ from yolox.utils.visualize import plot_tracking
 # import BYTETracker
 from bytetrack import BYTETracker
 
-# load yolox model and weights here
-# COMMENT: these parameters are hardcoded we might wanna offload
-#          offload them into a separate config file
+# import ViFi-CLIP
+from vificlip.utils.config import get_config
+from vificlip.trainers import vificlip
+from vificlip.datasets.pipeline import Compose
+
+################################################################
+# load yolox model and weights here                            #
+# COMMENT: these parameters are hardcoded, might wanna offload #
+#          offload them into a separate config file            #
+################################################################
 test_size = (512, 512)
 test_conf = 0.01
 nmsthre = 0.65
@@ -28,21 +37,24 @@ model.load_state_dict(ckpt["model"])
 model.cuda()
 model.half()
 model.eval()
-print('COCO-pretrained yolox-nano loaded')
 
 # yolox utils
 preproc = ValTransform(legacy=False)
 
-# Configure depth and color streams
+###############################################
+# Configure realsense depth and color streams #
+###############################################
 pipeline = rs.pipeline()
 config = rs.config()
 
 width, height = 1280, 720
 config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
 
-print('Realsense camera stream loaded')
+cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
 
-# load BYTETracker
+####################
+# load BYTETracker #
+####################
 aspect_ratio_thresh = 0.6
 min_box_area = 10
 track_thresh = 0.5
@@ -57,7 +69,65 @@ tracker = BYTETracker(track_thresh=track_thresh,
 					  fuse_score=fuse_score,
 					  frame_rate=frame_rate)
 
-# Start streaming
+################################################################
+# load ViFi-CLIP, config and weights here                      #
+# COMMENT: these parameters are hardcoded, might wanna offload #
+#          offload them into a separate config file            #
+################################################################
+class_names = ['punch','kick','walk']
+
+class parse_option():
+    def __init__(self):
+        self.config = 'vificlip/configs/zero_shot/train/k400/16_16_vifi_clip.yaml'
+        self.output =  ''
+        self.resume = 'weights/vifi_clip_10_epochs_k400_full_finetuned.pth'
+        # No need to change below args.
+        self.only_test = True
+        self.opts = None
+        self.batch_size = None
+        self.pretrained = None
+        self.accumulation_steps = None
+        self.local_rank = 0
+vargs = parse_option()
+vconfig = get_config(vargs)
+
+vmodel = vificlip.returnCLIP(vconfig,
+                            class_names=class_names,)
+vmodel.cuda()
+vmodel.eval()
+vmodel.half()
+
+vckpt = torch.load(vconfig.MODEL.RESUME, map_location='cpu')
+load_state_dict = vckpt['model']
+# now remove the unwanted keys:
+if "module.prompt_learner.token_prefix" in load_state_dict:
+	del load_state_dict["module.prompt_learner.token_prefix"]
+
+if "module.prompt_learner.token_suffix" in load_state_dict:
+	del load_state_dict["module.prompt_learner.token_suffix"]
+
+if "module.prompt_learner.complete_text_embeddings" in load_state_dict:
+	del load_state_dict["module.prompt_learner.complete_text_embeddings"]
+
+new_state_dict = OrderedDict()
+for k, v in load_state_dict.items():
+	name = k[7:] # remove `module.`
+	new_state_dict[name] = v
+
+vmodel.load_state_dict(new_state_dict, strict=False)
+
+###################
+# Start streaming #
+###################
+# init global buffer to collect frames
+global_buffer = Queue(32)
+# init local buffer to collect metadata from each tracked ID
+# COMMENT: choose between limited/unlimited number of local buffers
+#          limited to 8 local buffers for now
+local_buffers = []
+for i in range(20):
+	local_buffers[i] = Queue(32)
+
 pipeline.start(config)
 with torch.no_grad():
 	while True:
@@ -68,13 +138,15 @@ with torch.no_grad():
 			continue
 
 		# Convert images to numpy arrays then to torch tensors in GPU
+		# COMMENT: choose a format to collect the frames (numpy array? torch tensor? BGR? RGB?)
 		img = np.asanyarray(color_frame.get_data()) #BGR?? RGB?? Double check if channels are correct
+		global_buffer.put(img)
 		img_ori = img
 		ratio = min(test_size[0] / height, test_size[1] / width)
-		
+
 		img, _ = preproc(img, None, test_size)
 		img = torch.from_numpy(img).unsqueeze(0).float().half().cuda()
-		
+
 		# inference
 		outputs = model(img)
 		outputs = postprocess(outputs,
@@ -82,12 +154,12 @@ with torch.no_grad():
 							  test_conf,
 							  nmsthre,
 							  class_agnostic=True)
-		
+
+		# BYTETracker
 		if outputs[0] is not None:
 			output = outputs[0].cpu()
 			output = output[output[:, 6] == 0] # filter out ONLY persons
-			
-			#BYTETracker
+
 			online_targets = tracker.update(output, [height, width], test_size)
 			online_tlwhs = []
 			online_ids = []
@@ -100,17 +172,24 @@ with torch.no_grad():
 					online_tlwhs.append(tlwh)
 					online_ids.append(tid)
 					online_scores.append(t.score)
+					local_buffers[tid] = tlwh # push to local buffers here
+				
 			# Plot BYTETracker output
 			# print(online_tlwhs) use this to get scrops for tracked object
-			vis_res = plot_tracking(img_ori,
-									online_tlwhs,
-									online_ids)
+			# COMMENT: marked for removal, collect metadata instead
+			#vis_res = plot_tracking(img_ori, online_tlwhs, online_ids)
 		else:
-			vis_res = img_ori
+			#vis_res = img_ori
+			
+		# COMENT: add local buffer collection and processing here
 
-		# Show images
-		cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-		cv2.imshow('RealSense', vis_res)
+		# Only show images if processing is completed on buffer
+		# COMMENT: show images sequentially and flush all buffers
+		if global_buffer.full():
+			cv2.imshow('RealSense', vis_res)
+			global_buffer.clear()
+			for local_buffer in local_buffers:
+				local_buffer.clear()
 		if cv2.waitKey(1) & 0xff == ord('q'):
 			break
 
